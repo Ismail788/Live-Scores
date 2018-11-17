2403363183367ba27f8a3ff363cbfb18919c97aa
Ismail788 <ismail.soliman788@gmail.com>
2018-11-18 00:00:09 +0100

    this a new change

--- src/App.js
+++ src/App.js
@@ -3,57 +3,23 @@ import Form from './Components/form';
 import Countries from './Components/country';
 import Fixtures from './Components/fixtures';
 import Scores from './Components/scores';
-
-const Demo_key = '6r94GgdPiJ5ciqdx';
-const Demo_secret = '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U';
-
+import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      countries: [],
-      isloaded: false,
-    }
   }
-  componentDidMount(){
-     fetch(`http://livescore-api.com/api-client/countries/list.json?key=${Demo_key}&secret=${Demo_secret}`)
-     .then((response)=>response.json())
-     .then((json)=>
-        {
-       this.setState({
-         isloaded:true,
-         countries:json,
-       })
-     })
-  }
-render() {
-  let  {isloaded, countries} = this.state;
-  if(!isloaded){
-    return<div>loading...</div>
-  }else {
+
+  render() {
     return (
       <div calss='App'>
-      <h1>Live Scores</h1>
-      <ul>
-      {countries.data.country.map(country=>(
-        <li key={country.id}>
-        name:{country.name}
-
-        </li>
-      ))};
-
-    </ul>
-    <Form  form={this.state.form} />
-    <Countries  Countries = {this.state.countries}/>
-    <Scores  Scores = {this.state.scores} />
-    <Fixtures  fixtures = {this.state.fixtures}/>
- </div>
-
- );
+        <Form />
+        <Countries />
+        <Scores />
+        <Fixtures />
+      </div>
+    );
+  }
 }
 
-  }
-
-  }
 export default App;
--- src/Components/country.js
+++ src/Components/country.js
@@ -3,47 +3,52 @@ import React, { Component } from 'react';
 const Demo_key = '6r94GgdPiJ5ciqdx';
 const Demo_secret = '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U';
 
-
 class Countries extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       countries: [],
       isloaded: false,
     }
   }
-  componentDidMount(){
-     fetch(`http://livescore-api.com/api-client/countries/list.json?key=${Demo_key}&secret=${Demo_secret}`)
-     .then((response)=>response.json())
-     .then((json)=>
-        {
-       this.setState({
-         isloaded:true,
-         countries:json,
-       })
-     })
+
+  componentDidMount() {
+    fetch(`http://livescore-api.com/api-client/countries/list.json?key=${Demo_key}&secret=${Demo_secret}`)
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          isloaded: true,
+          countries: json,
+        })
+      })
   }
-render() {
-  let  {isloaded, countries} = this.state;
-  if(!isloaded){
-    return<div>loading...</div>
-  }else {
+
+  onCountrySelect = e => {
+    e.preventDefault();
+    const countryId = e.target.value;
+  }
+
+  render() {
+    let { isloaded, countries } = this.state;
+
+    if (!isloaded) {
+      return <div>loading...</div>
+    }
+
     return (
       <div calss='Country'>
-      <h1>All Countries</h1>
-      <ul>
-      {countries.data.country.map(country=>(
-        <li key={country.id}>
-        name:{country.name}
-        </li>
-      ))};
-   </ul>
- </div>
-
- );
-}
-
-}
-
+        <h1>All Countries</h1>
+        <select onChange={this.onCountrySelect}>
+          {
+            countries.data.country.map(country => (
+              <option key={country.id} value={country.id}>
+                {country.name}
+              </option>
+            ))
+          }
+        </select>
+      </div>
+    );
   }
+}
 export default Countries;
